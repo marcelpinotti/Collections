98457d3dc21f1cb6bb934d9ff44cb9e4641d66d6
marcelpinotti <marcel.pinotti@gmail.com>
2021-12-26 00:52:20 -0300

    atualizando branch

--- src/main/java/IniciandoComparableComparators.java
+++ src/main/java/IniciandoComparableComparators.java
@@ -12,32 +12,32 @@ public class IniciandoComparableComparators {
         estudantes.add(new Estudante("Pedro", 19));
         estudantes.add(new Estudante("Carlos", 23));
         estudantes.add(new Estudante("Mariana", 21));
-        estudantes.add(new Estudante("Jo�o", 18));
+        estudantes.add(new Estudante("João", 18));
         estudantes.add(new Estudante("Thiago", 20));
         estudantes.add(new Estudante("George", 22));
         estudantes.add(new Estudante("Larissa", 21));
 
-        System.out.println(estudantes); // --> ordem de inser��o
+        System.out.println(estudantes); // --> ordem que foram inseridos os objetos
 
         estudantes.sort((first, second) -> first.getIdade() - second.getIdade()); // Lambda
-        System.out.println(estudantes); // --> ordem natural dos n�meros
+        System.out.println(estudantes); // --> ordem natural dos números
 
         estudantes.sort((first, second) -> second.getIdade() - first.getIdade()); // Lambda
-        System.out.println(estudantes); // --> ordem reversa dos n�meros
+        System.out.println(estudantes); // --> ordem reversa dos números
 
-        estudantes.sort(Comparator.comparingInt(Estudante::getIdade)); // m�todo reference
-        System.out.println(estudantes); // --> ordem natural dos n�meros
+        estudantes.sort(Comparator.comparingInt(Estudante::getIdade)); // método reference
+        System.out.println(estudantes); // --> ordem natural dos números
 
-        estudantes.sort(Comparator.comparingInt(Estudante::getIdade).reversed()); // m�todo reference
-        System.out.println(estudantes); // --> ordem reversa dos n�meros
+        estudantes.sort(Comparator.comparingInt(Estudante::getIdade).reversed()); // método reference
+        System.out.println(estudantes); // --> ordem reversa dos números
 
         Collections.sort(estudantes);  // interface Comparable
 
-        System.out.println(estudantes); // --> ordem natural dos n�meros
+        System.out.println(estudantes); // --> ordem natural dos números
 
         Collections.sort(estudantes, new Estudante()); // interface Comparator
 
-        System.out.println(estudantes); // --> ordem reversa dos n�meros
+        System.out.println(estudantes); // --> ordem reversa dos números
 
     }
 }
